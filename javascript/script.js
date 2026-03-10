1476782817b99bfe5e0812d97f8ce49cdeea9799
Marcos127 <78666407+Marcos127@users.noreply.github.com>
2026-03-10 19:33:25 -0300

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,32 @@
+
+
+ScrollReveal().reveal('#home', {
+    origin: 'left',
+    duration: 5000,
+    distance: '20%'
+});
+
+ScrollReveal().reveal('#about', {
+    origin: 'left',
+    duration: 5000,
+    distance: '12%'
+});
+
+ScrollReveal().reveal('#menu', {
+    origin: 'right',
+    duration: 5000,
+    distance: '12%'
+});
+
+ScrollReveal().reveal('#review', {
+    origin: 'left',
+    duration: 5000,
+    distance: '12%'
+});
+
+ScrollReveal().reveal('#address', {
+    origin: 'right',
+    duration: 5000,
+    distance: '12%'
+});
+
